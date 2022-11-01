8a765337d7db7e9196a2766d14d37c6ae1bd88dd
Christopher Marinica <chris.marinica@gmail.com>
2022-11-01 17:54:32 -0400

    commit tree ee5b9251eaccae980e233ef9e14c4c65a636ef60

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const firstName = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards(firstName,event){
+    let message = []
+      for(let i = 0; i < firstName.length; i++) {
+        message.push(`Thank you, ${firstName[i]}, for the wonderful ${event} gift!`);
+      }  
+return message;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--
+    }
+}
+[countDown()];
+
